fa415a1e6abcad6fe5e1c1225670ee63b99dfd06
Dan DiGangi <me@dandigangi.com>
2023-11-27 18:47:16 -0600

    init

---
+++ data-structures/arrays.js
@@ -0,0 +1,57 @@
+//
+// array = list of items
+// array notation = brackets [ ]
+//
+
+// create a cart array with nothing in it
+// because nothing, the length (aka size) is 0
+// const cart = [];
+
+// create the cart array with items in it
+// the length is 3
+// const cart = ['apple', 'cat', 'dog'];
+
+// get the total amount of items in the array (length)
+// console.log(cart.length);
+
+// this can be used in meaningful ways
+// imagine we dont want someone to checkout on Amazon.com if there cart is empty
+// we can check that
+// if (cart.length > 0) {
+//   console.log('has cart items, checking out');
+//   console.log('total items in cart', cart.length);
+// }
+
+// arrays can be accessed using the position of items in them
+// the positions are at an index
+// indexes are a number referencing each position of an item
+// it starts and 0 and counts until the last item
+// use brackets to access an array and pass it a number
+// const cart = ['apple', 'cat', 'dog'];
+
+// // logs apple
+// console.log(cart[0]);
+// // logs dog
+// console.log(cart[2]);
+// // logs what?
+// console.log(cart[5]);
+
+// // update a value
+// cart[0] = 'banana';
+// console.log(cart[0]);
+
+// store just about anything in array
+// const cart = [4, 'asdfsd', {}, function () {}];
+
+// console.log(cart);
+
+/*
+    ! Challenge
+    * create a variable named octCohort
+    * include names of several of your peers (at least 4)
+    // * nest an array with an instructor names (Paul, Henry, and Mary)
+    * access Mary's name and reassign to potato
+    * access Paul's name and reassign to HeadPotato
+    ! Spicey Mode
+    // * remove the last entry within the nested array
+*/
---
+++ data-structures/misc.js
@@ -0,0 +1,7 @@
+//
+console.log(name);
+var name = 'paul';
+console.log(name);
+
+var test; // this is a value of undefined
+console.log(test);
