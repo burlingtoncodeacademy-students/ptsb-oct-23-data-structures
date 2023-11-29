222de7e1ac4b6cbd855ce79ca18b5a2efb0d63c5
Dan DiGangi <me@dandigangi.com>
2023-11-29 16:43:12 -0600

    object student part

--- object-methods.js
+++ object-methods.js
@@ -1,1 +1,66 @@
-// IN PROGRESS
+//
+// ? Lesson: Object Methods
+//
+// * Basics:
+// * Functions that are built-in and can only be called on them (similar to how arrays have their own)
+// * Has access to the global scope and can reference itself using "this"
+//
+
+//
+// ? Part #2:
+// * Intermediate object access and using methods
+//
+
+// * Reading objects stored data
+// * {LIVE_EXAMPLES}
+
+// * Checking objects for properties
+// * {LIVE_EXAMPLES}
+
+// * Other built-in object methods to know
+// * create()
+// * keys()
+// * values()
+// * fromEntries()
+// * is()
+// * {LIVE_EXAMPLES}
+
+// * Executing object methods
+// * {LIVE_EXAMPLES}
+
+// * Adding custom methods to objects
+// * {LIVE_EXAMPLES}
+
+/////////////////////////
+
+// ? Challenge #1:
+// * Available at end of this lesson's section
+// * {CLASS_CODE_TIME}
+
+/////////////////////////
+
+//
+// ? Part #3:
+// * Advanced concepts
+//
+
+// * Chaining object methods
+// * {LIVE_EXAMPLES}
+
+// * How to build a chainable method in an object
+// * {LIVE_EXAMPLES}
+
+/////////////////////////
+
+// ? Challenge #2:
+// * Available at end of this lesson's section
+// * {CLASS_CODE_TIME}
+
+/////////////////////////
+
+//
+// * Homework
+// * Menu Order      - https://replit.com/team/ptsb-oct-2023/menu-order
+// * GPA Calculator  - https://replit.com/team/ptsb-oct-2023/gpa-calculator
+// * Tag Search      - https://replit.com/team/ptsb-oct-2023/tag-search (optional)
+//
--- objects.js
+++ objects.js
@@ -1,78 +1,78 @@
 //
-// Lesson: Objects
-// Everything in Javascript's data types family is technically an object. This is specifically learning the object type.
+// ? Lesson: Objects
+// * Everything in Javascript's data types family is technically an object. This is specifically learning the object type.
 //
-// Basics:
-// - Container for values in an almost JSON format
-// - Denoted by { }
-// - Not indexable and order is not guaranteed like arrays
+// ? Basics:
+// * - Container for values in an almost JSON format
+// * - Denoted by { }
+// * - Not indexable and order is not guaranteed like arrays
 //
-// Data/Methods:
-// Stored as properties in key/value pairs and has built-in methods.
-// Values can be any valid Javascript type including functions.
-// -- someObject.{myProperty}    |  Gives the value of myProperty in the object (more access ways below)
-// -- someObject.{someMethod}()  |  Access/run a method on the object
+// ? Data/Methods:
+// * Stored as properties in key/value pairs and has built-in methods.
+// * Values can be any valid Javascript type including functions.
+// * -- someObject.{myProperty}    |  Gives the value of myProperty in the object (more access ways below)
+// * -- someObject.{someMethod}()  |  Access/run a method on the object
 //
-// Built-in methods and custom can be added, accessed, chained, and executed
-// -- someObject.hasOwnProperty()                   |  Built-in method, is executed
-// -- someObject.myCustomMethod()                   |  Custom, is executed
-// -- someObject.myCustomMethod                     |  Custom, is accessed
-// -- someObject.hasOwnProperty().myCustomMethod()  |  Chaining, is running multiple methods on the same object, the object is returned from each method to the next
+// ? Built-in methods and custom can be added, accessed, chained, and executed
+// * -- someObject.hasOwnProperty()                   |  Built-in method, is executed
+// * -- someObject.myCustomMethod()                   |  Custom, is executed
+// * -- someObject.myCustomMethod                     |  Custom, is accessed
+// * -- someObject.hasOwnProperty().myCustomMethod()  |  Chaining, is running multiple methods on the same object, the object is returned from each method to the next
 //
-// Advanced:
-// - Has access to the "this" keyword which deals with current scope.
-// - Closures are how Javascript handles scope and is related to "this".
-// - Custom objects (properties/methods) can be manipulated in different ways such as "private" limiting access and usage.
+// ? Advanced:
+// * - Has access to the "this" keyword which deals with current scope.
+// * - Closures are how Javascript handles scope and is related to "this".
+// * - Custom objects (properties/methods) can be manipulated in different ways such as "private" limiting access and usage.
 //
-// Docs:
-// JSON    - https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
-// Objects - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+// * Docs:
+// * JSON    - https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+// * Objects - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
 //
 
 /////////////////////////
 
 //
-// Part #1:
-// Creating Object Literals
+// ? Part #1:
+// * Creating Object Literals
 //
 
-// Creating Objects
-// {LIVE_EXAMPLES}
+// * Creating objects (literal and constructor versions)
+// * {LIVE_EXAMPLES}
 
-// Accessing Objects - Dot Notation (more common)
-// More commonly used
-// {LIVE_EXAMPLES}
+// * Adding properties to objects
+// * {LIVE_EXAMPLES}
 
-// Accessing Objects - Keys (less common)
-// When it might be used is a specific string key is required but invalid to dot notation
-// {LIVE_EXAMPLES}
+// * Modifying object properties
+// * {LIVE_EXAMPLES}
 
-// Reading object keys and values
-// {LIVE_EXAMPLES}
+// * Accessing objects - dot notation (more common)
+// * More commonly used
+// * {LIVE_EXAMPLES}
 
-// Checking objects for properties
-// {LIVE_EXAMPLES}
+// * Accessing objects - key based (less common)
+// * When it might be used is a specific string key is required but invalid to dot notation
+// * {LIVE_EXAMPLES}
 
-// Adding properties
-// {LIVE_EXAMPLES}
-
-/////////////////////////
-
-// Challenge #1:
-// Available at end of this lesson's section
-// {CLASS_CODE_TIME}
+// * Iterating (looping) over objects
+// * {LIVE_EXAMPLES}
 
 /////////////////////////
 
-// Challenge #2:
-// Available at end of this lesson's section
-// {CLASS_CODE_TIME}
+// ? Challenge #1:
+// * Available at end of this lesson's section
+// * {CLASS_CODE_TIME}
+
+/////////////////////////
+
+// ? Challenge #2:
+// * Available at end of this lesson's section
+// * {CLASS_CODE_TIME}
 
 /////////////////////////
 
 //
-// Homework
-// Menu Order      - https://replit.com/team/ptsb-oct-2023/menu-order
-// GPA Calculator  - https://replit.com/team/ptsb-oct-2023/gpa-calculator
-// Tag Search      - https://replit.com/team/ptsb-oct-2023/tag-search (optional)
+// * Homework
+// * Menu Order      - https://replit.com/team/ptsb-oct-2023/menu-order
+// * GPA Calculator  - https://replit.com/team/ptsb-oct-2023/gpa-calculator
+// * Tag Search      - https://replit.com/team/ptsb-oct-2023/tag-search (optional)
 //
