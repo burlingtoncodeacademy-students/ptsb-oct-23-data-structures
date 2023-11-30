c60e176244061f448668a6a76591e371039c9512
Dan DiGangi <me@dandigangi.com>
2023-11-30 15:53:32 -0600

    oops

--- classes-methods.js
+++ classes-methods.js
@@ -4,118 +4,10 @@
     * can be used on any class instance
 */
 
-class Account {
-  constructor(name, ssn, balance) {
-    this.name = name;
-    this.ssn = ssn;
-    this.balance = balance;
-    this.acctNo = this.generateAcctNo();
-  }
-
-  generateAcctNo() {
-    const min = 1000000000;
-    const max = 9999999999;
-    const randNum = Math.floor(Math.random() * (max - min + 1) + min);
-    return randNum;
-  }
-
-  deposit(amt) {
-    this.balance += amt;
-  }
-
-  withdraw(amt) {
-    return this.balance < amt
-      ? 'Insufficient Funds'
-      : (this.balance -= amt);
-  }
-}
-
-const joey = new Account('Joey', 123456789, 1000);
-console.log(joey);
-const julia = new Account('Julia', 222232323, 500000);
-console.log(julia);
-julia.withdraw(125000);
-console.log(julia);
-
 /*
     ? Class Inheritance
     * extends a parent class with a child class
 */
-
-class MinorAccount extends Account {
-  constructor(name, ssn, balance, isMinor) {
-    // allows us to access properties and methods of a parent class
-    super(name, ssn, balance);
-    this.isMinor = isMinor;
-  }
-
-  waiveOverdraft() {
-    this.balance += 35;
-  }
-}
-
-const bentley = new MinorAccount('Bentley', 111111111, 50, true);
-console.log(bentley);
-bentley.waiveOverdraft();
-console.log(bentley);
-
-/*
-    ? Factory Functions
-    * functions that accept data and return new instance of a Class
-*/
-
-class Circle {
-  constructor(radius) {
-    typeof radius === 'number'
-      ? (this.radius = radius)
-      : new Error('Radius must be a number');
-  }
-
-  circumference() {
-    return 2 * Math.PI * this.radius;
-  }
-
-  area() {
-    return Math.PI * this.radius ** 2;
-  }
-}
-
-const myCircle = new Circle(20);
-console.log(myCircle.area());
-console.log(myCircle.circumference());
-
-function circleFromDiameter(diameter) {
-  return new Circle(diameter / 2);
-}
-
-const myDiaCircle = circleFromDiameter(20);
-console.log(myDiaCircle);
-
-/*
-    ? Static Methods
-    * typically provided as utilities/helpers to an application
-    * do not require instantiation with a new operator
-    * create an instance of our class through its method
-*/
-class CircleStaticFactoryFx {
-  static createCircleUsingDiameter(diameter) {
-    return new CircleStaticFactoryFx(diameter / 2);
-  }
-
-  constructor(radius) {
-    typeof radius === 'number'
-      ? (this.radius = radius)
-      : new Error('Radius must be a number');
-  }
-}
-
-const circleFromClass = new CircleStaticFactoryFx(20);
-// TypeError -> not accessible on instance level -> static method not visible
-// console.log(circleFromClass.createCircleUsingDiameter())
-
-// Accessing CircleStaticFactoryFx using "pass by reference" -> static method is visible
-const diaCircle = CircleStaticFactoryFx.createCircleUsingDiameter(40);
-console.log(diaCircle);
 
 /*
     ? Static Fields
@@ -129,42 +21,17 @@ console.log(diaCircle);
 // Static fields must be the first thing in a class if they need to be declared
 // Remember that classes are not objects but templates for them
 // In a class this is how it expects the template to be designed vs what an object expects
-class Dog = {
-  type = 'pet'
-  constructor() {}
-}
 
-// You can only use strings, numbers, and falsy values (null, undefined)
+// You can only use strings, numbers, falsy values (null, undefined), or nothing (see line #140)
 // Objects with no value when assigning are undefined
-class Dog {
-  bio;
-  type = 'pet';
-
-  getType() {
-    const typeObject = { type: this.type };
-    return typeObject;
-  }
-}
-
-const jesse = new Dog();
-console.log(jesse);
-console.log(jesse.bio); // Returns undefined
-console.log(jesse.type);
-console.log(jesse.getType());
-
-jesse.bio = '13/10 she is a great doggo, bork bork';
-console.log(jesse.bio);
 
 /*
-  ? Private Methods
+  ? Private Fields
+  * Syntax: ???
 */
 
 // Challenge
-// What do you think happens here? What reasons might we want private properties?
-class Dog {
-  #type = 'pet'
-  constructor() {}
-}
-
-const ralf = new Dog();
-ralf.#type = 'parent';
+// Research private fields with Javascript classes
+// Create a private field named type with a value of pet
+// Create a new instance of Dog and change the private field value to something new
+// What do you think happens aft? What reasons might we want private properties?
--- classes.js
+++ classes.js
@@ -3,6 +3,7 @@
     * templates or blueprints for creating objects
     * they encapsulate data with code
     * JS is a prototype-based langauge with access to OOP principles
+    * Class are NOT hoisted
     * Classes are considered special functions. They can be defined as:
         * class declarations
         * class expressions
@@ -17,63 +18,24 @@
 // Classes look similar to objects using curly braces but they are not
 // Consider them templates for objects that you will later create
 // Constructors is a special method that create/initializes an object created by a class (1 per class)
-class Learner {
-  constructor(name, city, cohort) {
-    this.name = name;
-    this.city = city;
-    this.cohort = cohort;
-    this.scholarships = 7500;
-  }
-}
 
 // ? Create a new instance
 // We create our instance, aka instantiation of the class, and pass in properties designed in our class's constructor
-const khale = new Learner('Bob', 'Brownsville', 'sdb-sep-23');
-console.log(khale);
 
 // Challenge
 // If log the Kelly instance what would the values be for name, city, and cohort?
 // Would this error?
-const khale = new Learner('Kelly');
 
 // ? Prototype Chaining
 // Consider the "instance" again, it's a clone more or less of its parent class which is why typeof below logs object
 // Class = template for objects, instances = objects created by using the class following the template
-console.log(typeof bob);
-console.log(bob instanceof Learner);
-console.log(bob instanceof Object);
-console.log(bob instanceof Object);
 
 // ? Accessing object instance values
 // We can access, add, and update things just like our previous lesson
-bob.scholarships = 10000;
-bob.graduated = 'in progress';
-console.log(bob);
 
 // ? Class Expression
 // Remember this no different than assigning a variable
-const House = class {
-  constructor(address, bedrooms, bathrooms) {
-    this.address = address;
-    this.bedrooms = bedrooms;
-    this.bathrooms = bathrooms;
-  }
-};
-
-let LakeViewEast = new House('1007 Orange St', 3, 2);
 
 // ? Named Class Expression
 // Equivalent to the above but has a name that can be referenced
 // Think about how a function can also be declared similarly - function(){} or function getHome(){}
-const apartment = class Aparment {
-  constructor(address, bedrooms, bathrooms) {
-    this.address = address;
-    this.bedrooms = bedrooms;
-    this.bathrooms = bathrooms;
-  }
-};
-
-let Mila = new House('1007 Orange St', 3, 2);
-
-console.log(LakeViewEast);
-console.log(Mila);
