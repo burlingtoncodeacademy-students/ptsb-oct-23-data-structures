2d9a49672e11e4b783068a43d52bfa414b288494
Dan DiGangi <me@dandigangi.com>
2023-11-30 18:16:29 -0600

    David

--- classes.js
+++ classes.js
@@ -18,13 +18,24 @@
 // Classes look similar to objects using curly braces but they are not
 // Consider them templates for objects that you will later create
 // Constructors is a special method that create/initializes an object created by a class (1 per class)
+class Learner {
+  constructor(name, city, cohort) {
+    this.name = name;
+    this.city = city;
+    this.cohort = cohort;
+  }
+}
 
 // ? Create a new instance
 // We create our instance, aka instantiation of the class, and pass in properties designed in our class's constructor
+const bob = new Learner('Bob', 'Chicago', 'ptsb-oct-23');
+const kelly = new Learner();
+
+console.log(bob);
+console.log(kelly);
 
 // Challenge
-// If log the Kelly instance what would the values be for name, city, and cohort?
-// Would this error?
+// In the constructor if there is no name or cohort, it throw or logs an error
 
 // ? Prototype Chaining
 // Consider the "instance" again, it's a clone more or less of its parent class which is why typeof below logs object
